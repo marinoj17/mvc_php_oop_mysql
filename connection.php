00823493ef2d5a837c8f4016e2581bbc27c5d19c
marinoj17 <marcosjavier17@gmail.com>
2022-10-22 03:08:48 -0300

    implementacion de demo crud oop php mysql

--- connection.php
+++ connection.php
@@ -1,9 +1,4 @@
 <?php
-	/**
-	* Conexión a la base de datos
-	* Autor: Elivar Largo
-	* Sitio Web: wwww.ecodeup.com
-	*/
 	class Db
 	{
 		private static $instance=NULL;
@@ -15,7 +10,7 @@
 		public static function getConnect(){
 			if (!isset(self::$instance)) {
 				$pdo_options[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
-				self::$instance= new PDO('mysql:dbname=heroku_0246d367bae1c3d;host=us-cdbr-east-06.cleardb.net','bd62f65351bd78','28bc1d89',$pdo_options);
+				self::$instance= new PDO('mysql:host=us-cdbr-east-06.cleardb.net;dbname=heroku_0246d367bae1c3d','bd62f65351bd78','28bc1d89',$pdo_options);
 			}
 			return self::$instance;
 		}
