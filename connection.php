ba04d0b2c3fdb9f70955cf2d043822c3e5801a21
marinoj17 <marcosjavier17@gmail.com>
2022-10-22 01:20:36 -0300

    implementacion de demo crud mvc oop php con integracion mysql

--- connection.php
+++ connection.php
@@ -15,7 +15,7 @@
 		public static function getConnect(){
 			if (!isset(self::$instance)) {
 				$pdo_options[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
-				self::$instance= new PDO('mysql:host=localhost;dbname=mvc','root','',$pdo_options);
+				self::$instance= new PDO('mysql:host=us-cdbr-east-06.cleardb.net;dbname=heroku_0246d367bae1c3d','bd62f65351bd78','28bc1d89',$pdo_options);
 			}
 			return self::$instance;
 		}
